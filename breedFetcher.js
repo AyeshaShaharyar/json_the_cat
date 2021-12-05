e3e8dcd1182b72aa88ed1ee52cd1672242315c99
AyeshaShaharyar <ayesha2598@gmail.com>
2021-12-05 23:50:11 +0000

    tests for breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
-const args = process.argv.slice(2);
 const request = require("request");
-request(
-  `https://api.thecatapi.com/v1/breeds/search/?q=${args}`,
-  (error, response, body) => {
-    const Objdata = JSON.parse(body);
 
-    if (Objdata.message) {
-      return console.log("Error Found");
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`,
+    (error, response, body) => {
+      const Objdata = JSON.parse(body);
+
+      if (Objdata.message) {
+        return callback(Objdata.message, null);
+      } else if (Objdata.length === 0) {
+        return callback("Invalid breed: breed not found", null);
+      } else {
+        return callback(null, Objdata[0].description);
+      }
     }
+  );
+};
 
-    if (Objdata.length === 0) {
-      return console.log("Breed not found");
-    }
-
-    console.log(Objdata[0].description);
-  }
-);
+module.exports = { fetchBreedDescription };
